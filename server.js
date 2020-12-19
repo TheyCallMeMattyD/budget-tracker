cfb4a71f3aa8d9b1aaec57c33b61657d2783a678
TheMattyDGWU <themattydgwu@gmail.com>
2020-12-19 11:03:31 -0500

    update process environment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -19,9 +20,9 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
     useNewUrlParser: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
   }
 );
 
